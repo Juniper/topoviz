577d4f077168573321d22d09b2b320a0ff7d5e26
Chris Jenn <crj@juniper.net>
2020-12-02 20:24:28 +1100

    changing to MIT license

--- host_replace_ospf.pl
+++ host_replace_ospf.pl
@@ -1,12 +1,33 @@
 #!/usr/bin/perl 
-
-# Copyright (c) 2019, Juniper Networks, Inc
-# All rights reserved
-# This SOFTWARE is licensed under the LICENSE provided in the
-# ./LICENCE file. By downloading, installing, copying, or otherwise
-# using the SOFTWARE, you agree to be bound by the terms of that
-# LICENSE.
-
+#
+# Copyright (c) Juniper Networks, Inc., 2019-2020. All rights reserved.
+#
+# Notice and Disclaimer: This code is licensed to you under the MIT License
+# (the "License"). You may not use this code except in compliance with the License.
+# This code is not an official Juniper product.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Third-Party Code: This code may depend on other components under separate
+# copyright notice and license terms. Your use of the source code for those
+# components is subject to the terms and conditions of the respective license
+# as noted in the Third-Party source code file.
+#
 
 use strict;
 use warnings;
